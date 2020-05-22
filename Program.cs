2fc1e75b7827c6f2300e4de9964d3129b498123f
Kristen Howton <kristen.howton@gmail.com>
2020-05-22 13:40:35 -0500

    intial commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Hello {
+    class Program {
+        //Starting pint of the application, look for main method
+        static void Main (string[] args) {
+            //what will run
+            Console.WriteLine ("Hello World!");
+            SayHi("Bubba");
+
+        }
+
+        static void SayHi(string name) 
+        {
+            Console.WriteLine($"Hi, {name}");
+            
+        }
+    }
+}
